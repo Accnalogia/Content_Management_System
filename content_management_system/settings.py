568eb753279ae7f8d891a8f4f09b566581f4e3f0
Accnalogia <bhagyeshkurlekar@gmail.com>
2020-12-23 16:38:38 +0530

    Config Change: added api.core app in installed apps and changed the database settings to sqlite from postgres.

--- content_management_system/settings.py
+++ content_management_system/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.core'
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,8 @@ WSGI_APPLICATION = 'content_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'CMS',
-        'USER': 'postgres',
-        'PASSWORD': 12345,
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
