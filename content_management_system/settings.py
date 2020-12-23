19c4f8a6e3128b88a2ee8cb3928ce4ebc170a6ad
Accnalogia <bhagyeshkurlekar@gmail.com>
2020-12-23 15:50:55 +0530

    Removing Secret_Key

--- content_management_system/settings.py
+++ content_management_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mtz9v^r*f-2v&+4a0q0i_tq+(3#vr&lr7tp7*i0i^hjp_j=4vg'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,13 +73,17 @@ WSGI_APPLICATION = 'content_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CMS',
+        'USER': 'postgres',
+        'PASSWORD': 12345,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
